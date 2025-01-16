e513c616e855e9bb4c2228d6c28c5c2c59465f55
Ayemo-AndrewGold <ayemoandrewgold@gmail.com>
2025-01-16 09:35:25 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import Banks from './Components/Banks/Banks'
+import About from './Components/About/About'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className='container'>
+        <Title subTitle='Our Programs' title='What We Offer' /> 
+        <Programs />
+        <About />
+        <Title subTitle='Gallery' title='Bank Photo' />
+        <Banks />
+        <Title subTitle='Testimonial' title='What Client says' />
+        <Testimonials />
+        {/* <Title subTitle='Contact Us' title='Get intouch' />
+        <Contact /> */}
+      </div>
     </div>
   )
 }
